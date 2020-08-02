39f6d26372479b63993e5c5bd00e6bb58a533f11
Lucy Wan <lucy.w@berkeley.edu>
2020-08-02 00:21:06 -0700

    ERawre

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
 
 
 function geoExp(){
-
 	var input = document.getElementById('geoExpInp').value
-	var output2 = "<p>E[x] = </p>$\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$"+"<br><p>E[x] = 1/"+input+"<br>E[x] = " + (1/parseInt(input)).toString() + "</p>";
-	var output = "$$\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$$"
+	var output = "E[x] = \\(\\frac{1}{p}\\)<br>E[x] = \\(\\frac{1}{"+input+"}\\)<br>E[x] = " + (1/parseInt(input)).toString();
 	var el = document.getElementById("geoExpOut");
 	el.innerHTML = output;
-	MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+	MathJax.typesetPromise([el])
+      	
+
+	// MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
         }
 
 
 function geoVar(){
-	var input = document.getElementById('geoExpInp').value
-	var output = "Var[x] = 1/p<br>E[x] = 1/"+input+"<br>Var[x] = " + (1/parseInt(input)).toString();
+	var input = document.getElementById('geoVarInp').value
+	var output = "E[x] = \\(\\frac{1}{p}\\)<br>E[x] = \\(\\frac{1}{"+input+"}\\)<br>E[x] = " + (1/parseInt(input)).toString();
+	var el = document.getElementById("geoVarOut");
+	el.innerHTML = output;
+	MathJax.typesetPromise([el])
+      	
 
-	document.getElementById("geoExpOut").innerHTML = output;
+	// MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
         }
